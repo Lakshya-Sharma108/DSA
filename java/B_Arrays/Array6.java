5fbe5f866fbee596807a2ac77f9aecc2382b1260
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-12 17:03:39 +0530

    Add Array6.java to demonstrate the structure and access of 2D arrays

---
+++ java/B_Arrays/Array6.java
@@ -0,0 +1,26 @@
+package B_Arrays;
+
+public class Array6 {
+    public static void main(String[] args) {
+        /*
+            2D Arrays:
+
+            How a 2-dimensional array looks like:
+
+                                col0   col1   col2
+                           row0   2      3      4
+            int[] arr =    row1   5      6      7
+                           row2   9      1      8
+
+            If you want to access a value => arr[row][col];
+            arr[0][1] = 3
+            arr[2][2] = 8
+
+         */
+
+
+
+
+
+    }
+}
