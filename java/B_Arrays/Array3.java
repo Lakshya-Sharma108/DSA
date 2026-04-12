a9c0ebfef62331b60167f44555fd52b1709233ac
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-12 16:31:50 +0530

    Add Array3.java for array multiplication example

---
+++ java/B_Arrays/Array3.java
@@ -0,0 +1,18 @@
+package B_Arrays;
+
+public class Array3 {
+    public static void main(String[] args) {
+
+
+//        Printing the multiplication of array values:
+        int[] arr = {2, 3, 4};
+        int multi = 1;
+        for (int value: arr){
+            multi *= value;
+        }
+        System.out.println("The multiplication of array values is: "+multi);
+
+
+
+    }
+}
