65457a44c1a9b413df873e239e7187fcad6c9db2
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-12 16:46:09 +0530

    Add Array4.java to find the maximum element in an array

---
+++ java/B_Arrays/Array4.java
@@ -0,0 +1,23 @@
+package B_Arrays;
+
+public class Array4 {
+    public static void main(String[] args) {
+
+//        Find the max element in array
+        int[] arr = {2, 6, -3, 15, 1, 8};
+
+        int max = 0;
+        for (int i = 0; i <= arr.length-1; i++) {
+            if (i > 0){
+                if (arr[i] > arr[max]){
+                    max = i;
+                }
+            }
+        }
+        System.out.println("The max is: "+arr[max]);
+        System.out.println("The index is: "+max);
+
+
+
+    }
+}
