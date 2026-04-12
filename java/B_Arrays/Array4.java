a004d483fe16b61246cf329242824796dc68e46c
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-12 16:48:45 +0530

    Update Array4.java to include an additional element in the array and simplify max element calculation

--- java/B_Arrays/Array4.java
+++ java/B_Arrays/Array4.java
@@ -4,15 +4,13 @@ public class Array4 {
     public static void main(String[] args) {
 
 //        Find the max element in array
-        int[] arr = {2, 6, -3, 15, 1, 8};
+        int[] arr = {2, 6, -3, 15, 1, 8, 22};
 
         int max = 0;
         for (int i = 0; i <= arr.length-1; i++) {
-            if (i > 0){
                 if (arr[i] > arr[max]){
                     max = i;
                 }
-            }
         }
         System.out.println("The max is: "+arr[max]);
         System.out.println("The index is: "+max);
