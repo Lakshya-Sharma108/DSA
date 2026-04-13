857c86e3cc869e61dfaa039371c0e980c6f6b13a
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-13 21:18:17 +0530

    Add String1.java to demonstrate string and character data types in Java

---
+++ java/C_Strings/String1.java
@@ -0,0 +1,26 @@
+package C_Strings;
+
+public class String1 {
+    public static void main(String[] args) {
+        /*
+            What is string?
+            String is a collection of characters inside "double quotes".
+            In java String is represented by String data type
+
+            What is character?
+            anything inside 'single quote' is a character.
+            In java character is represented by char data type
+        */
+
+
+//        Creation of string: using string literals
+        String str = "lakshaya";
+
+//        String creation using new keyword
+        String str2 = "sharma";
+
+
+
+
+    }
+}
