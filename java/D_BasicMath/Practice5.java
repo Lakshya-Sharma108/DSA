c75a87485d6f4b1a392573c3568c4b105638c2aa
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-14 16:18:07 +0530

    Optimize prime number check in Practice5.java for improved performance

--- java/D_BasicMath/Practice5.java
+++ java/D_BasicMath/Practice5.java
@@ -8,18 +8,27 @@ public class Practice5 {
 //        Prime number:
 //        A number which should be greater than 1 and only Divisible by 1 or itself
 
-        System.out.println(isPrime(17));
+//        To make it faster we can iterate till -> square root of num or (i*i <= n)
+
+        System.out.println(isPrime(12));
 
 
     }
 
 
     static boolean isPrime(int num){
-        for (int i = 2; i <= num-1; i++) {
+//        Faster version
+        for (int i = 2; i*i <= num; i++) {
             if (num%i == 0){
                 return false;
             }
         }
+
+//        for (int i = 2; i <= num-1; i++) {
+//            if (num%i == 0){
+//                return false;
+//            }
+//        }
 
         return true;
     }
