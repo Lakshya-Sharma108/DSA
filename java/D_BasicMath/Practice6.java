d6e87eee5878e4e73345ad7a671d76d1cc107aa1
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-16 16:24:30 +0530

    Implement GCD calculation in Practice6.java with user input

--- java/D_BasicMath/Practice6.java
+++ java/D_BasicMath/Practice6.java
@@ -1,8 +1,26 @@
 package D_BasicMath;
 
+import java.util.Scanner;
+
 public class Practice6 {
     public static void main(String[] args) {
 
+//        GCD (Greatest Common Divisor) or HCF (Highest Common Factor)
+//        Formula = gcd(a, b) = gcd(b, a%b)
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = input.nextInt();
+        System.out.print("Enter b: ");
+        int b = input.nextInt();
+
+        while (b != 0){
+            int oldValueOfb = b;
+            b = a%b;
+            a = oldValueOfb;
+        }
+
+        int ans = a;
+        System.out.println("The greatest common divisor is: "+ans);
     }
 }
