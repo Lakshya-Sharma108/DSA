f2c3b6e4af77f5d73cabdb5a94643b07dc12bb56
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-02 12:51:44 +0530

    Add BitwiseOp.java to demonstrate bitwise operators and their functionality

---
+++ java/E_BitwiseOperator/BitwiseOp.java
@@ -0,0 +1,48 @@
+package E_BitwiseOperator;
+
+public class BitwiseOp {
+    public static void main(String[] args) {
+        /*
+        Bitwise Operator:
+        Bitwise operator works on bit level
+
+        bit -> binary -> 0's and 1's
+
+        Integer = 4 bytes
+        1 byte = 8 bits
+
+
+        Example: <- how it stored in memory
+        int num1 = 5;
+        int num2 = 6;
+
+        binary of 5 -> 101
+        binary of 6 -> 110
+
+        num1 = 00000000 00000000 00000000 00000101
+        num2 = 00000000 00000000 00000000 00000110
+               ^                                 ^
+        Most significant bit            Most significant bit
+
+
+        <> Because Bitwise Operator operates on bit level, it makes them fast
+
+
+        Operator    Name
+        &           AND
+        |           OR
+        ^           XOR
+        ~           NOT
+        <<          LEFT SHIFT
+        >>          RIGHT SHIFT
+        >>>         UNSIGNED RIGHT SHIFT
+
+
+        & (AND): if both operands true (1), then only output will be true (1) otherwise false
+        | (OR): if any one of the operand is true (1), then output will be true (1) otherwise false
+        ^ (XOR): if both operands are same (1, 1 or 0, 0), then output will be false otherwise ture
+
+
+    */
+    }
+}
