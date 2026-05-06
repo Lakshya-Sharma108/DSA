031f62ff5d10d252f28917d5c607f4f4b54adba2
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-06 12:50:20 +0530

    Add Practice3.java to demonstrate checking if a number is a power of 2 using bitwise operations

---
+++ java/E_BitwiseOperator/Practice3.java
@@ -0,0 +1,10 @@
+package E_BitwiseOperator;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        // Power of 2 with second formula --
+
+        // Formula:
+        // if   n & (n-1) == 0   then number is power of 2
+    }
+}
