256134a6d48961bec4e51575287d85d7aa57e670
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-06 13:29:22 +0530

    Add average method in Practice1.java to calculate the average of array elements

--- java/F_ArrayProblems/Part1/Practice1.java
+++ java/F_ArrayProblems/Part1/Practice1.java
@@ -5,6 +5,23 @@ public class Practice1 {
 
         // Find the average of array elements
 
+        // Formula:
+        // Average = sum of all elements / total number of elements
 
+        int[] arr = {2, 4, 1, 3};
+        System.out.println("Average: "+average(arr));
+
+    }
+
+    public static double average(int[] arr){
+
+        double sum = 0;
+        int size = arr.length;
+        for (int num : arr){
+            sum += num;
+        }
+
+        double average = sum / size;
+        return average;
     }
 }
