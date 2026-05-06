6e38e79664a4ec0d3ac1041260464f0f989a2f25
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-06 14:10:01 +0530

    Add logic to Practice7.java for identifying the first unsorted element in both ascending and descending arrays

--- java/F_ArrayProblems/Part1/Practice7.java
+++ java/F_ArrayProblems/Part1/Practice7.java
@@ -4,5 +4,36 @@ public class Practice7 {
     public static void main(String[] args) {
 
         // Find 1st unsorted element
+
+        // first array
+        //int[] arr = {2, 4, 6, 8, 9, 2, 10, 12};
+
+        // second array
+        int[] arr = {12, 10, 8, 6, 10, 4, 2, 1};
+
+
+        // Checking if array is starting ascending order
+        if (arr[1] > arr[0]){
+            // code for ascending order
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[i-1]){
+                    System.out.println("The first unsorted element is: "+arr[i]);
+                    System.out.println("The index of element is: "+i);
+                }
+            }
+
+        }
+
+        // Checking if array is starting descending order
+        if (arr[1] < arr[0]) {
+            // code for descending order
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > arr[i-1]){
+                    System.out.println("The first unsorted element is: "+arr[i]);
+                    System.out.println("The index of element is: "+i);
+                }
+            }
+
+        }
     }
 }
