3538b576097a55dff856d33b95061c61c032716d
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-07 21:55:12 +0530

    Add Practice3.java to identify elements with highest and lowest frequency

---
+++ java/F_ArrayProblems/Part2/Practice3.java
@@ -0,0 +1,17 @@
+package F_ArrayProblems.Part2;
+
+import java.util.Arrays;
+
+public class Practice3 {
+    public static void main(String[] args) {
+
+        // Identify elements with highest and lowest frequency
+
+        int[] arr = {10, 20, 10, 10, 30, 20, 30, 50, 30, 60, 50};
+
+        int[] arr2 = new int[arr.length];
+
+
+
+    }
+}
