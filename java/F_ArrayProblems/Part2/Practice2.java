2f578f6e955a6af3bc8eb35708e2a95f867ae426
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-07 16:39:13 +0530

    Add Practice2.java to demonstrate shifting array elements by one position

---
+++ java/F_ArrayProblems/Part2/Practice2.java
@@ -0,0 +1,18 @@
+package F_ArrayProblems.Part2;
+
+import java.util.Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+
+        // Shift array element by one position
+
+        // i/p -> [10, 20, 30, 40, 50]
+
+        // o/p -> [ 50,  10,  20,  30,  40 ]
+        //           |   ->   ->   ->   ->
+        //           |
+        //    Last element repeats from start
+
+    }
+}
