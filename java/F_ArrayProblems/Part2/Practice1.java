688dddc1101a3db758b2b1a600d476b38f466053
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-07 16:31:59 +0530

    Enhance Practice1.java to implement array reversal using the two-pointer technique

--- java/F_ArrayProblems/Part2/Practice1.java
+++ java/F_ArrayProblems/Part2/Practice1.java
@@ -5,9 +5,23 @@ import java.util.Arrays;
 public class Practice1 {
     public static void main(String[] args) {
 
-        // Reverse an array
+        // Reverse an array  -- two pointer technique
 
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
 
+        int i = 0;
+        int j = (arr.length-1);
+
+        while (i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
