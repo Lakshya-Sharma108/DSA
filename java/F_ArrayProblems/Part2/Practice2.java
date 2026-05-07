17c3e05d2b5fdd254ade3cd9511663d672685bb7
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-07 17:10:42 +0530

    Add logic to Practice2.java for shifting array elements to the right

--- java/F_ArrayProblems/Part2/Practice2.java
+++ java/F_ArrayProblems/Part2/Practice2.java
@@ -14,5 +14,16 @@ public class Practice2 {
         //           |
         //    Last element repeats from start
 
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int temp = arr[arr.length-1];
+
+        for (int i = (arr.length-1); i > 0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+
+        System.out.println(Arrays.toString(arr));
+
     }
 }
