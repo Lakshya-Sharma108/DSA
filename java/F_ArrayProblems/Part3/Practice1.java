3ec5090df9b93dcaa36f9436abc663cb6eaf633b
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-12 13:28:54 +0530

    Add Practice1.java to implement sorting of an array of 0's and 1's

---
+++ java/F_ArrayProblems/Part3/Practice1.java
@@ -0,0 +1,14 @@
+package F_ArrayProblems.Part3;
+
+import java.util.Arrays;
+
+public class Practice1 {
+    public static void main(String[] args) {
+
+        // Given an array of 0's and 1's, sort the array so that all 0's are placed before 1's
+
+        // input  -> {0, 1, 1, 0, 1, 0, 0}
+        // output -> {0, 0, 0, 0, 1, 1, 1}
+
+    }
+}
