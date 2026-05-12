22a73021403be59c1d89386b48ff98841ff5b6e6
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-12 12:59:26 +0530

    Add logic to Practice3.java to calculate and display frequency of elements

--- java/F_ArrayProblems/Part2/Practice3.java
+++ java/F_ArrayProblems/Part2/Practice3.java
@@ -1,13 +1,35 @@
 package F_ArrayProblems.Part2;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Practice3 {
     public static void main(String[] args) {
 
         // Identify elements with highest and lowest frequency
 
-        int[] arr = {10, 20, 10, 10, 30, 20, 30, 50, 30, 60, 50};
+        int[] arr = {10, 20, 10, 10, 30, 20, 30, 50, 10, 30, 60, 50};
+
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+
+        int maxFreq = -1;
+        int maxFreqKey = -1;
+
+        int minFreq = -1;
+        int minFreqKey = -1;
+
+        for (int num : arr){
+            frequency.put(num, frequency.getOrDefault(num, 0)+1);
+
+            if (frequency.get(num) > maxFreq){
+                maxFreq = frequency.get(num);
+                maxFreqKey = num;
+            }
+        }
+
+        System.out.println(frequency);
+        System.out.println("Max frequency key is: "+maxFreqKey+" With frequency: "+maxFreq);
+        System.out.println("Min frequency key is: "+minFreqKey+" With frequency: "+minFreq);
 
 
     }
