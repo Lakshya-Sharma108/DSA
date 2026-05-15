fd6a2e665396218804ab348db3ca5bebca7b4e10
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-15 21:01:35 +0530

    Add initial setup for union of arrays in Practice4.java

--- java/F_ArrayProblems/Part3/Practice4.java
+++ java/F_ArrayProblems/Part3/Practice4.java
@@ -1,8 +1,19 @@
 package F_ArrayProblems.Part3;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Practice4 {
     public static void main(String[] args) {
 
         // Union of Arrays
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {4, 5, 6, 7, 8};
+
+        Set<Integer> unionSet = new LinkedHashSet<>();
+
+
+
     }
 }
