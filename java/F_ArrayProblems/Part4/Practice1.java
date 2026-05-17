a26d2dec933763219dfe25e68e6e1748f38fd4a7
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-17 21:18:34 +0530

    Add Practice1.java to implement two sum functionality

---
+++ java/F_ArrayProblems/Part4/Practice1.java
@@ -0,0 +1,8 @@
+package F_ArrayProblems.Part4;
+
+public class Practice1 {
+    public static void main(String[] args) {
+
+        // Two sum -- find a pair whose sum is equal to targeted value
+    }
+}
